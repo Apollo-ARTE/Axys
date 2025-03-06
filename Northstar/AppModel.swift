75f553747a4324586a75a0e3f798dd67d88d8472
Alessandro Bortoluzzi <info@bortoluzzi.dev>
2025-03-06 09:23:06 +0100

    Initial commit

---
+++ Northstar/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  Northstar
+//
+//  Created by Alessandro Bortoluzzi on 06/03/25.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
