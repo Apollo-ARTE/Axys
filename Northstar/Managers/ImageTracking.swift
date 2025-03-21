d950b4a039e24d1abfed4976c96f14d02732e02a
Alessandro Bortoluzzi <info@bortoluzzi.dev>
2025-03-21 11:04:11 +0100

    Start tracking on step confirm

---
+++ Northstar/Managers/ImageTracking.swift
@@ -0,0 +1,61 @@
+//
+//  ImageTracking.swift
+//  Image Calibration
+//
+//  Created by Alessandro Bortoluzzi on 05/03/25.
+//
+
+import ARKit
+import RealityKit
+
+@MainActor
+@Observable
+class ImageTracking {
+	let session = ARKitSession()
+
+	var planeAnchors: [UUID: ImageAnchor] = [:]
+	var entityMap: [UUID: Entity] = [:]
+
+	var centerEntity: Entity
+	var movableEntity: ModelEntity
+
+	var modelPosition: SIMD3<Float> = .zero
+
+	let imageInfo = ImageTrackingProvider(
+		referenceImages: ReferenceImage.loadReferenceImages(inGroupNamed: "images")
+	)
+
+	init() {
+		centerEntity = ModelEntity.centerSphere()
+		movableEntity = ModelEntity.movableSphere()
+
+		centerEntity.addChild(movableEntity)
+		movableEntity.position.y = 0.001
+
+		startTracking()
+	}
+
+	func startTracking() {
+		if ImageTrackingProvider.isSupported {
+			Task {
+				try await session.run([imageInfo])
+				for await update in imageInfo.anchorUpdates {
+					updateImage(update.anchor)
+					modelPosition = movableEntity.position
+				}
+			}
+		}
+	}
+
+	func updateImage(_ anchor: ImageAnchor) {
+		if planeAnchors[anchor.id] == nil {
+			// Add a new entity to represent this image.
+			entityMap[anchor.id] = centerEntity
+			planeAnchors[anchor.id] = anchor
+		}
+
+		if anchor.isTracked {
+			entityMap[anchor.id]?.transform = Transform(matrix: anchor.originFromAnchorTransform)
+		}
+	}
+}
