b9f35a61b94befd4ee74bdd8bef164ded08a3356
Alessandro Bortoluzzi <info@bortoluzzi.dev>
2025-05-13 12:34:39 +0200

    Add NavigationStack to home view

--- Northstar/Views/HomeView.swift
+++ Northstar/Views/HomeView.swift
@@ -15,44 +15,51 @@ struct HomeView: View {
 	@State private var isConnected = false
 
     var body: some View {
-		VStack {
-			List {
-				Section {
-					LabeledContent {
-						Text("Off")
-					} label: {
-						Text("Rhino Connection")
-						Text("Connect to your local network and run the Rhino Plugin")
-							.font(.footnote)
-					}
-					LabeledContent {
-						Text("0")
-					} label: {
-						Text("Imported Models")
-						Text("Run the plugin on Rhino and select your models")
-							.font(.footnote)
-					}
-					Toggle(isOn: $isConnected) {
-						Text("Calibration")
-						Text("Calibrate your models with real world coordinates")
-							.font(.footnote)
-					}
-					.onChange(of: isConnected) {
-						if isConnected {
-							connectionManager.connectToWebSocket()
-						} else {
-							connectionManager.disconnectFromWebSocket()
+		NavigationStack {
+			VStack {
+				List {
+					Section {
+						LabeledContent {
+							Text("Off")
+						} label: {
+							Text("Rhino Connection")
+							Text("Connect to your local network and run the Rhino Plugin")
+								.font(.footnote)
 						}
+						NavigationLink {
+							Text("Models view")
+						} label: {
+							LabeledContent {
+								Text("0")
+							} label: {
+								Text("Imported Models")
+								Text("Run the plugin on Rhino and select your models")
+									.font(.footnote)
+							}
+						}
+
+						Toggle(isOn: $isConnected) {
+							Text("Calibration")
+							Text("Calibrate your models with real world coordinates")
+								.font(.footnote)
+						}
+						.onChange(of: isConnected) {
+							if isConnected {
+								connectionManager.connectToWebSocket()
+							} else {
+								connectionManager.disconnectFromWebSocket()
+							}
+						}
+					} header: {
+						header
+							.padding(.bottom, 32)
+					} footer: {
+						footer
+							.padding(.top, 32)
 					}
-				} header: {
-					header
-						.padding(.bottom, 32)
-				} footer: {
-					footer
-						.padding(.top, 32)
 				}
+				.scrollBounceBehavior(.basedOnSize)
 			}
-			.scrollBounceBehavior(.basedOnSize)
 		}
 		.padding(32)
 		.frame(width: 550, height: 550)
