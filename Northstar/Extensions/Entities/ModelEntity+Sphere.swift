077c512a70a7e9d90ae397164cdb863d5bcfe398
Alessandro Bortoluzzi <info@bortoluzzi.dev>
2025-05-22 09:46:41 +0200

    Disable moving in calibration spheres

--- Northstar/Extensions/Entities/ModelEntity+Sphere.swift
+++ Northstar/Extensions/Entities/ModelEntity+Sphere.swift
@@ -11,10 +11,6 @@ import UIKit
 extension ModelEntity {
 	static func movableSphere() -> ModelEntity {
 		let sphere = ModelEntity(mesh: .generateSphere(radius: 0.01), materials: [UnlitMaterial(color: .white)])
-		sphere.components.set(InputTargetComponent())
-		sphere.components.set(HoverEffectComponent())
-		sphere.generateCollisionShapes(recursive: true)
-		sphere.name = "movableSphere"
 		return sphere
 	}
 }
